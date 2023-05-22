ee28c1703255ffd022b0843af3666c6600659404
vihha <helge.hofstad@vitecsoftware.com>
2023-05-22 09:50:08 +0200

    ser ut til å funke

--- cli.js
+++ cli.js
@@ -1,119 +1,134 @@
 #!/usr/bin/env node
 'use strict';
 
-const meow = require('meow');
 const chalk = require('chalk');
-const exec = require('child_process').exec;
+const yargs = require('yargs/yargs');
+const decompress = require('decompress');
+const path = require('path');
 
-const greenInverse = chalk.bold.inverse.green;
-const cyanInverse = chalk.bold.inverse.cyan;
-const magentaInverse = chalk.bold.inverse.magenta;
-const yellowInverse = chalk.bold.inverse.yellow;
-const grayInverse = chalk.bold.inverse.gray;
+const {findInstance, findTemplate, config, openExplorer} = require('.');
 
-const {findInstance, findTemplate, config} = require('.');
+function handleInstanceCommand(params) {
+    if (params.verbose)
+        console.log('handling INSTANCE command', params.guid);
+    return findInstance(params.guid, params)
+        .then(result => {
+            if (params.verbose)
+                console.info(chalk.green('\nFound IDML file: ' + result));
+            return result;
+        })
+        .then(idmlPath => {
+            if (params.unpack) {
+                if (params.verbose)
+                    console.log('Unpacking instance!');
+                const unzipPath = `C:\\TEMP\\INSTANCES\\${params.guid}\\`;
+                return decompress(idmlPath, unzipPath)
+                    .then(_ => unzipPath);
+            }
+            return idmlPath;
+        })
+        .then(pathToOpen => {
+            return openExplorer(pathToOpen)
+                .then(msg => console.log('\n' + chalk.green(msg)));
+        })
+        .catch(function (err) {
+            console.error(chalk.red('\nAn error occurred! (' + err.message + ')'));
+            process.exit(1);
+        });
+}
 
-// const meowHelper = require('./meow-helper');
-// const help = meowHelper();
-// const cli = meow(help.text, help.flags);
+function handleTemplateCommand(params) {
+    if (params.verbose)
+        console.log('handling TEMPLATE command', params);
+    return findTemplate(params.path, params)
+        .then(result => {
+            if (params.verbose)
+                console.info(chalk.green('\nFound TEMPLATE: ' + result));
+            return result;
+        })
+        .then(idmlPath => {
+            if (params.unpack) {
+                if (params.verbose)
+                    console.log('Unpacking template!');
+                const unzipPath = path.resolve(`C:\\TEMP\\TEMPLATES\\${params.path}\\`);
+                return decompress(idmlPath, unzipPath)
+                    .then(_ => unzipPath);
+            }
+            return idmlPath;
+        })
+        .then(pathToOpen => {
+            if (params.verbose)
+                console.log('Opening folder ' + pathToOpen);
+            return openExplorer(pathToOpen)
+                .then(msg => console.log('\n' + chalk.green(msg)));
+        })
+        .catch(function (err) {
+            console.error(chalk.red('\nAn error occurred! (' + err.message + ')'));
+            process.exit(1);
+        });
+}
 
-const cli = meow(chalk`
-    ${greenInverse('Usage')}
-      {gray $} {green fox} {cyan <command>} {magenta <parameter>} {yellow [options]}
-
-    ${cyanInverse('Commands')}
-      {cyan instance} ${chalk.dim('Locates an *instance* from GUID')}
-      {cyan template} ${chalk.dim('Locates a *template* from Template partial path')}
-      {cyan config} ${chalk.dim('Set base path for <environment>')}
-
-    ${magentaInverse('Parameter')}
-      ${chalk.dim('The GUID or Template to find (or base path if command is config)')}
- 
-    ${yellowInverse('Options')}
-      {yellow --environment, -e} ${chalk.dim('Which environment to use (e.g. STAGE or PROD). Default is STAGE')}
-      {yellow --verbose, -v} ${chalk.dim('More output')}
- 
-    ${grayInverse('Examples')}
-      $ fox instance 6607e477-326b-4713-b520-596701d25e20
-      $ fox instance 6607e477-326b-4713-b520-596701d25e20 --environment=PROD --verbose
-      $ fox template 61/VITEC_DEMO/Generell_salgsoppgavemal
-      $ fox template 61/VITEC_DEMO/Generell_salgsoppgavemal --environment=PROD
-      $ fox config \\\\10.10.141.30\\Publish\\IDS\\
-`, {
-    flags: {
-        environment: {
-            type: 'string',
-            default: 'STAGE',
-            alias: 'e'
-        },
-        verbose: {
-          type: 'boolean',
-          default: false,
-          alias: 'v'
-        }
-    }
-});
-
-function setConfig(value, options) {
-    if (!value) {
+function handleConfigCommand(params) {
+    if (params.verbose)
+        console.log('handling CONFIG command', params);
+    if (!params.path) {
         console.log('Current config: ', config.get('environments'));
         return;
     }
     if (!config.get('environments'))
         config.set('environments', {prod: null, stage: null});
-    if (!options.env || options.env.toLowerCase() === 'stage') {
-        if (verbose) console.log('Setting new base path for STAGE: ' + value);
-        config.set('environments.stage', value);
+    if (params.environment.toLowerCase() === 'stage') {
+        if (params.verbose) console.log('Setting new base path for STAGE: ' + params.path);
+        config.set('environments.stage', params.path);
     }
-    else if (options.env.toLowerCase() === 'prod') {
-        if (verbose) console.log('Setting new base path for PROD: ' + value);
-        config.set('environments.prod', value);
+    else if (params.environment.toLowerCase() === 'prod') {
+        if (params.verbose) console.log('Setting new base path for PROD: ' + params.path);
+        config.set('environments.prod', params.path);
     }
-}
-
-const actions = {
-    'instance': findInstance,
-    'template': findTemplate,
-    'config': setConfig
-};
-const command = cli.input[0];
-const parameter = cli.input[1];
-const env = cli.flags.environment;
-const verbose = cli.flags.verbose;
-const hasConfig = config && config.get('environments') && config.get(`environments.${env.toLowerCase()}`);
-const operation = actions[command];
-
-if (verbose) 
-    console.info('\ninputs: ', {command, parameter, env, verbose});
-
-if (!command || !actions[command])
-    console.error(chalk.red('Command does not exist (' + command + ')'));
-else if (!hasConfig) {
-    if (command === 'config')
-        operation(paramter, {env});
     else {
-        console.error('\nNo configuration found! You must call \'fox config\' and pass in a base path for the environment you are trying to access.');
-        if (verbose)
-            console.info('Current environment config: ', config.get('environments'));
+        console.log('Did not find a known environment: "' + params.environment + '". No value set.');
     }
 }
-else {
-    operation(parameter, {env})
-        .then(result => {
-            if (verbose)
-                console.info(chalk.green('\nFound folder: ' + result));
-            const explorer = exec(`explorer.exe /select,${result}`);
-            explorer.stderr.on('data', (data) => {
-              console.error(chalk.red('\nSomething went wrong opening the folder'), data.toString());
-              process.exit(1);
-            });
-            explorer.on('exit', code => {
-              console.log(chalk.green('\nSuccess!'));
-              process.exit();
-            });
-        })
-        .catch(err => {
-            console.error(chalk.red('\nError'), err.message ? err.message : err);
-            process.exit(1);
-        });
-}
+
+
+var argv = yargs(process.argv.slice(2))
+  .scriptName('fox')
+  .option('e', {
+    alias: ['env', 'environment'],
+    default: 'STAGE',
+    type: 'string',
+    global: true
+  })
+  .boolean('u')
+  .alias('u', 'unpack')
+  .default('u', false)
+  .global('u')
+  .boolean('v')
+  .alias('v', 'verbose')
+  .default('v', false)
+  .global('v')
+  .command('instance <guid>', 'Locates an *instance* from GUID', function (yargs) {
+    return yargs.positional('guid', {
+      type: 'string'
+    }).example([
+        ['$0 instance 6607e477-326b-4713-b520-596701d25e20', 'Find the instance with GUID 6607e477-326b-4713-b520-596701d25e20'],
+        ['$0 instance 1ae6a737-442a-4fea-a4c1-40f91db5038a -e PROD', 'Find the instance with GUID 1ae6a737-442a-4fea-a4c1-40f91db5038a in PROD environment']
+    ])
+  }, handleInstanceCommand)
+  .command('template <path>', 'Locates a *template* from Template partial path', function (yargs) {
+    return yargs.positional('path', {
+        type: 'string'
+    }).example([
+        ['$0 template 61/VITEC_DEMO/Generell_salgsoppgavemal', 'Find the template with path 61/VITEC_DEMO/Generell_salgsoppgavemal for environment specified with -e']
+    ])
+  }, handleTemplateCommand)
+  .command('config [path]', 'Set (or get) base path for environment', function (yargs) {
+    return yargs.positional('path', {
+        type: 'string'
+    }).example([
+        ['$0 config', 'Outputs the config values for every environment'],
+        ['$0 config \\\\10.10.141.30\\Publish\\IDS\\', 'Sets base path for STAGE environment']
+    ])
+  }, handleConfigCommand)
+  .help('help')
+  .argv
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const Conf = require('conf');
+const exec = require('child_process').exec;
+const chalk = require('chalk');
 
 const {findInstance, findTemplate} = require('./lib');
 
 const config = new Conf();
 
+const openExplorer = function (result) {
+    return new Promise((res, rej) => {
+        const explorer = exec(`explorer.exe /select,${result}`);
+        explorer.stderr.on('data', (data) => {
+            throw new Error('An error occurred while trying to access ' + result);
+        });
+        explorer.on('exit', code => {
+          res('Success!');
+        });
+    });
+};
+
 exports = module.exports = {
     findInstance,
     findTemplate,
-    config
+    config,
+    openExplorer
 }
